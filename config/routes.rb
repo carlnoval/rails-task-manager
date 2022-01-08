69457d5727018e57530d228ad322f2171d90a5f4
Carl Noval <cjrnoval22@gmail.com>
2022-01-08 15:15:22 +0900

    bare minimum show route-controller-view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  # READ
+  # READ - index
   get 'tasks', to: 'tasks#index'
+  # READ - show
+  # get 'tasks/:id', to: 'task#show', as 'restaurant'
+  get 'tasks/:id', to: 'tasks#show'
+
 end

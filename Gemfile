15ee98e330215e82804325d5f23ca192bb56c147
Carl Noval <cjrnoval22@gmail.com>
2022-01-08 15:03:40 +0900

    gem added: fontawesome, eased seeding with useful methods, completed tasks page route-controller-view

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'simple_form'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'faker'
+gem 'font-awesome-sass', '~> 5.15.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+def create_uncompleted_tasks(task_count)
+  task_count.times do
+    task_title = Faker::Verb.base.capitalize
+    task_details = "#{task_title} #{rand(1..10)} #{Faker::Game.title}"
+    new_task = Task.new(title: task_title, details: task_details)
+    new_task.save
+  end
+end
+
+def create_completed_tasks(task_count)
+  task_count.times do
+    task_title = Faker::Verb.base.capitalize
+    task_details = "#{task_title} #{rand(1..10)} #{Faker::Game.title}"
+    new_task = Task.new(title: task_title, details: task_details, completed: true)
+    new_task.save
+  end
+end
+
 puts "Deleting all Tasks..."
 Task.destroy_all
- 
-puts 'Create 10 Tasks...'
-10.times do
-  task_title = Faker::Verb.base.capitalize
-  task_details = "#{task_title} #{rand(1..10)} #{Faker::Game.title}"
-  new_task = Task.new(title: task_title, details: task_details)
-  new_task.save
-end
+puts 'Create 9 Tasks...'
+create_uncompleted_tasks(2)
+create_completed_tasks(4)
+create_uncompleted_tasks(2)
 puts 'Completed creating Tasks...'
+

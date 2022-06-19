4fbe59ff6a844a9801719a16e681ca1bcea084b4
Carl Noval <cjrnoval22@gmail.com>
2022-06-19 09:57:26 +0900

    testing for valid task

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 =begin
-This is a sample basic template
+This is a sample basic template, the '#truncated_details' test below fits this basic template
 
 RSpec.describe Class, type: :mvc_type do
   describe '#method' do
@@ -13,6 +13,14 @@ end
 =end
 
 RSpec.describe Task, type: :model do
+
+  describe '#initialize' do
+    it 'returns a valid Task' do
+      task = Task.new(title: "Rspec Task for testing", details: "12345678901234567890<-twenty characters")
+      expect(task.valid?).to eq(true)
+    end
+  end
+
   describe '#truncated_details' do
     it 'returns the first 16 characters + "..." of the task description' do
       task = Task.new(title: "Rspec Task for testing", details: "12345678901234567890<-twenty characters")

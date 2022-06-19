a597586a1fd962af7d601257cf1cfc34e950ecb4
Carl Noval <cjrnoval22@gmail.com>
2022-06-19 11:03:21 +0900

    minor adjust ments on test description and moved the require line

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 =begin
 This is a sample basic template, the '#truncated_details' test below fits this basic template
 
@@ -12,24 +10,26 @@ RSpec.describe Class, type: :mvc_type do
 end
 =end
 
+require 'rails_helper'
+
 RSpec.describe Task, type: :model do
   let!(:task) { Task.new(title: "Rspec Task for testing", details: "12345678901234567890<-twenty characters") }
 
-  describe '#initialize' do
-    context 'when valid' do
-      it 'returns a valid Task' do
-        expect(task.valid?).to eq(true)
-      end
-    end
-    
-    context 'when invalid' do
-      it 'returns an invalid Task without a title' do
-        task = Task.new(details: "12345678901234567890<-twenty characters")
-        task.valid?
-        expect(task.errors.messages[:title][0]).to eq("can't be blank")
-      end
+describe '#initialize' do
+  context 'when valid' do
+    it 'returns a valid Task' do
+      expect(task.valid?).to eq(true)
     end
   end
+  
+  context 'when invalid' do
+    it 'returns an error message' do
+      task = Task.new(details: "12345678901234567890<-twenty characters")
+      task.valid?
+      expect(task.errors.messages[:title][0]).to eq("can't be blank")
+    end
+  end
+end
 
   describe '#truncated_details' do
     it 'returns the first 16 characters + "..." of the task description' do

c74a56901ec6e154308ffd3f652611fa17b973d0
Carl Noval <cjrnoval22@gmail.com>
2022-06-19 10:30:59 +0900

    spec/models/task_spec.rb

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -13,10 +13,10 @@ end
 =end
 
 RSpec.describe Task, type: :model do
+  let!(:task) { Task.new(title: "Rspec Task for testing", details: "12345678901234567890<-twenty characters") }
 
   describe '#initialize' do
     it 'returns a valid Task' do
-      task = Task.new(title: "Rspec Task for testing", details: "12345678901234567890<-twenty characters")
       expect(task.valid?).to eq(true)
     end
 
@@ -29,7 +29,6 @@ RSpec.describe Task, type: :model do
 
   describe '#truncated_details' do
     it 'returns the first 16 characters + "..." of the task description' do
-      task = Task.new(title: "Rspec Task for testing", details: "12345678901234567890<-twenty characters")
       expect(task.truncated_details).to eq("1234567890123456...")
     end
   end

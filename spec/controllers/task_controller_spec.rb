d85d615cb24c3d93712f1d76aaf7f165ae8d1643
Carl Noval <cjrnoval22@gmail.com>
2022-06-21 20:50:34 +0900

    prep for controller testing

---
+++ spec/controllers/task_controller_spec.rb
@@ -0,0 +1,10 @@
+# spec/mvc_foler_name/class_spaec.rb
+require 'rails_helper'
+
+RSpec.describe TasksController, type: :controller do
+  describe '#create' do
+    it 'description of test' do
+      expect(value).to eq(expected_result)
+    end
+  end
+end

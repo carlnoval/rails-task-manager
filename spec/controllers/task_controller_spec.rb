43fd1f310f02625f07be78c23b1c933a42b82ae7
Carl Noval <cjrnoval22@gmail.com>
2022-06-23 01:10:10 +0900

    rspec controller test with passing and already refactored tests

--- spec/controllers/task_controller_spec.rb
+++ spec/controllers/task_controller_spec.rb
@@ -1,10 +1,21 @@
-# spec/mvc_foler_name/class_spaec.rb
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
   describe '#create' do
-    it 'description of test' do
-      expect(value).to eq(expected_result)
+    # post action has to be inside `before` otherwise it won't work 
+    before { post :create, params: { task: { title: 'My Task Title', details: 'Details for the task I created' } } }
+    # the task key is the task variable inside each `it` blocks
+    let!(:task) { Task.last }
+
+    it 'creates a new task' do
+      # params for Task will always be a hash
+      # inside the hash will be the fields that were permitted from model's strong params (see strong params from model)
+      # can also be seen by putting a `raise` on the top of create model then running `params.permit!.to_h[:model]`
+      expect(task.details).to eq('Details for the task I created')
+    end
+
+    it 'redirects to task show path' do
+      expect(response).to redirect_to(task_path(task))
     end
   end
 end
